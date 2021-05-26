4086ddef3b51658683416264fd1195c2ce23796f
Ilyas Ali <Ilyas.Ali@softwire.com>
2021-05-26 20:16:07 +0100

    new top domain dictionary

--- regexDomain.js
+++ regexDomain.js
@@ -18,7 +18,6 @@ const domainArray = emailArray.map(function(email) {
     return email.substring(ampersandIndex + 1);
 });
 
-
 // making a dictionary of domains and their respective frequencies
 const domainFrequencyDictionary = {};
 domainArray.forEach(function (domain) {
@@ -27,7 +26,7 @@ domainArray.forEach(function (domain) {
     } else {
         domainFrequencyDictionary[domain] = 1;
     }
-})
+});
 
 // sorting the domains by frequency
 const dictionaryArr = Object.keys(domainFrequencyDictionary);
@@ -36,26 +35,34 @@ dictionaryArr.sort(function (domainA, domainB) {
 
 });
 
-// printing top 10 domanins
+// Obtaining a frequency dictionary of the high level domains using the existing dictionary.
+const topDomainFrequencyDictionary = {};
+dictionaryArr.forEach(function (domain) {
+    let dotPosition = domain.indexOf('.');
+    let currentTopDomain = domain.substring(0, dotPosition);
+    if (topDomainFrequencyDictionary.hasOwnProperty(currentTopDomain)) {
+        topDomainFrequencyDictionary[currentTopDomain] += domainFrequencyDictionary[domain];
+    }
+    else topDomainFrequencyDictionary[currentTopDomain] = domainFrequencyDictionary[domain];
+})
+
+console.log(topDomainFrequencyDictionary);
+
+// printing top 10 domains
 console.log(dictionaryArr.slice(0, 10));
 
 // Making an array of just high level domains
 const domainArrayTop = domainArray.map(function (domain) {
-    let dotPosition = domain.indexOf('.');
-    return domain.substring(0, dotPosition);
-});
+     let dotPosition = domain.indexOf('.');
+     return domain.substring(0, dotPosition);
+ });
+//  console.log(domainArrayTop);
 
 // making a dictionary of the frequency of the high level domains.
-const topLevelDictionary = {};
-domainArrayTop.forEach(function (domain) {
-    if (topLevelDictionary.hasOwnProperty(domain)) {
-        topLevelDictionary[domain]++;
-    } else {
-        topLevelDictionary[domain] = 1;
-    }
-})
+//const topLevelDictionary = {};
 
-console.log(topLevelDictionary);
+
+console.log(topDomainFrequencyDictionary);
 
 
 // Asking the user to input frequency
@@ -64,15 +71,16 @@ let userFrequency;
 input.question(`Please enter frequency: `, freq => {
     userFrequency = freq;
 
-    user(userFrequency)
-    input.close()
+    user(userFrequency);
+    input.close();
 })
 
+// Github is updated (wooo!)
 
 // Printing the user specified domains 
 function user(userFrequency) {
     let frequentDomains = dictionaryArr.filter(function (domain) {
-        return domainFrequencyDictionary[domain] > userFrequency
+        return domainFrequencyDictionary[domain] > userFrequency;
 
     })
     console.log('Domains that appear more often than ' + userFrequency + ' times are: ' + frequentDomains.join(', '));
